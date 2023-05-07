daec1d7a3a1f66071b933a296dcf889b9358b037
Sukhvinder Singh <sukhvindersingh8315@gmail.com>
2023-05-07 15:55:47 +0530

    change

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const host = "https://sukhwinder.netlify.app/"
+const uploadURL = `${host}api/files`;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
